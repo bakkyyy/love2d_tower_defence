1e6741f7eecd594f98650a3af04cadac4fe483ac
bakkyyy <kostya-98@bk.ru>
2019-11-09 02:35:08 +0300

    Добавили Меню

--- menu.lua
+++ menu.lua
@@ -1,7 +1,20 @@
+local Utils = require 'utils'
+
 local Menu = { buttons = {}, screens = {} }
 
 function Menu:load(screens)
-    
+    self.screens = screens
+    logo = Utils.imageFromCache("assets/logo.png")
+    table.insert(self.buttons, {image = Utils.imageFromCache("assets/menu/menu2.png"), fn = function()
+        self.screens.changeScreen('game', self.screens)
+    end })
+    table.insert(self.buttons, {image = Utils.imageFromCache("assets/menu/menu3.png"), fn = nil })
+    table.insert(self.buttons, {image = Utils.imageFromCache("assets/menu/menu4.png"), fn = nil })
+    table.insert(self.buttons, {image = Utils.imageFromCache("assets/menu/menu5.png"), fn = function()
+        love.window.close()
+    end })
+    table.insert(self.buttons, {image = Utils.imageFromCache("assets/menu/menu.png"), fn = nil })
+    mesh = Utils.gradientMesh("vertical", {0.160784, 0.501961, 0.72549, 1}, {0.427451, 0.835294, 0.980392, 1}, {1, 1, 1, 1})
 end
 
 function Menu:update(dt)
@@ -9,7 +22,36 @@ function Menu:update(dt)
 end
 
 function Menu:draw(ww, wh)
+    love.graphics.draw(mesh, 0, 0, 0, ww, hh)
 
+    local cx = ww / 2
+    local cy = wh / 2
+
+    love.graphics.draw(logo, cx, wh/5, 0, 1.5*cx/logo:getWidth(), 1.5*cx/logo:getWidth(), 0.5*logo:getWidth(), 0.5*logo:getHeight())
+
+    local mx, my = love.mouse.getPosition()
+    local cursorY = 0
+    local hovered = false
+
+    for i, btn in ipairs(self.buttons) do
+        local bx = cx - 200 + 24
+        local by = 2*wh/5 + cursorY
+        local mouseOver = bx < mx and mx < bx + 360 and by < my and my < by + 105
+
+        if mouseOver then
+            love.graphics.draw(btn.image, cx, 2*wh/5, 0, 1, 1, 0.5*btn.image:getWidth())
+            hovered = true
+            if love.mouse.isDown(1) then
+                btn.fn()
+            end
+        end
+
+        cursorY = cursorY + 147
+    end
+
+    if not hovered then
+        love.graphics.draw(self.buttons[5].image, cx, 2*wh/5, 0, 1, 1, 0.5*self.buttons[1].image:getWidth())
+    end
 end
 
 return Menu
